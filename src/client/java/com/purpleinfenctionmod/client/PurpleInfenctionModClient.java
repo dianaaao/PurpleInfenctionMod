045bafadeeb8aba427a79dbd55561692c6ef0a11
dianaaao <ovcarovadiana035@gmail.com>
2026-07-24 16:51:51 +0300

    import Fabric template

---
+++ src/client/java/com/purpleinfenctionmod/client/PurpleInfenctionModClient.java
@@ -0,0 +1,10 @@
+package com.purpleinfenctionmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PurpleInfenctionModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
