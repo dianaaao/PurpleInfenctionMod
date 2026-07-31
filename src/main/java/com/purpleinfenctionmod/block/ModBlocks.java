c7350e88c904d7566849c6e6cdca6656560e4bb1
dianaaao <ovcarovadiana035@gmail.com>
2026-07-31 18:48:15 +0300

    add new generation, need fix

--- src/main/java/com/purpleinfenctionmod/block/ModBlocks.java
+++ src/main/java/com/purpleinfenctionmod/block/ModBlocks.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.MushroomBlock;
+import net.minecraft.block.MushroomPlantBlock;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -68,6 +70,27 @@ public class ModBlocks {
         )
     );
 
+    public static final Block INFECTED_MUSHROOM_HAT = registerBlock(
+        "infected_mushroom_hat",
+        new MushroomBlock(
+            FabricBlockSettings.copyOf(Blocks.RED_MUSHROOM_BLOCK).strength(2.0f, 62.9f)
+        )   
+    );
+
+    public static final Block INFECTED_BROWN_MUSHROOM = registerBlock(
+        "infected_brown_mushroom",
+        new MushroomBlock(
+            FabricBlockSettings.copyOf(Blocks.BROWN_MUSHROOM_BLOCK).strength(2.0f, 62.9f)
+        )   
+    );
+
+    public static final Block INFECTED_SMALL_MUSHROOM = registerBlock(
+        "infected_small_mushroom",
+        new MushroomPlantBlock(
+            FabricBlockSettings.copyOf(Blocks.BROWN_MUSHROOM).strength(2.0f, 62.9f), null
+        )   
+    );
+
     private static Block registerBlock(String name, Block block){
         Registry.register(
             Registries.BLOCK,
@@ -113,6 +136,17 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
             .register(entries -> entries.add(INFECTED_MUSHROOM_STEM));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
+            .register(entries -> entries.add(INFECTED_MUSHROOM_HAT));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
+            .register(entries -> entries.add(INFECTED_BROWN_MUSHROOM));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL)
+            .register(entries -> entries.add(INFECTED_SMALL_MUSHROOM));
+
+        
 
         PurpleInfenctionMod.LOGGER.info("Registering Mod Blocks for " + PurpleInfenctionMod.MOD_ID);
     }
