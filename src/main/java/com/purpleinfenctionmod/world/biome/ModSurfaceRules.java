fe3c58627dcaac159de55e71388248c0918cea27
IllyaEpik <epi99k@gmail.com>
2026-08-25 16:57:16 +0200

    added plants

--- src/main/java/com/purpleinfenctionmod/world/biome/ModSurfaceRules.java
+++ src/main/java/com/purpleinfenctionmod/world/biome/ModSurfaceRules.java
@@ -1,47 +1,42 @@
 package com.purpleinfenctionmod.world.biome;
 
 import com.purpleinfenctionmod.block.ModBlocks;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.math.VerticalSurfaceType;
+import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+import net.minecraft.util.math.VerticalSurfaceType;
 
 public class ModSurfaceRules {
+
     private static final MaterialRules.MaterialRule INFECTED_GRASS =
         MaterialRules.block(ModBlocks.INFECTED_GRASS.getDefaultState());
 
-private static final MaterialRules.MaterialRule INFECTED_DIRT =
-        MaterialRules.block(ModBlocks.INFECTED_DIRT.getDefaultState());
+    // Anything at/above this Y is treated as "surface" and skipped.
+    private static final int CAVE_CEILING_Y = 60;
 
-private static final MaterialRules.MaterialRule GRAVEL =
-        MaterialRules.block(Blocks.GRAVEL.getDefaultState());
+    public static MaterialRules.MaterialRule makeRules() {
 
-public static MaterialRules.MaterialRule makeRules() {
+        MaterialRules.MaterialCondition caveFloor =
+            MaterialRules.stoneDepth(
+                0,
+                false,
+                0,
+                VerticalSurfaceType.FLOOR
+            );
 
-    MaterialRules.MaterialCondition topBlock =
-            MaterialRules.stoneDepth(0, false, 0, VerticalSurfaceType.FLOOR);
+        MaterialRules.MaterialCondition underground =
+            MaterialRules.not(
+                MaterialRules.aboveY(YOffset.fixed(CAVE_CEILING_Y), 0)
+            );
 
-    MaterialRules.MaterialCondition dirtLayer =
-            MaterialRules.stoneDepth(1, true, 4, VerticalSurfaceType.FLOOR);
-
-    MaterialRules.MaterialCondition isAboveWater =
-            MaterialRules.water(-1, 0);
-
-    MaterialRules.MaterialRule landRule = MaterialRules.sequence(
-            MaterialRules.condition(topBlock, INFECTED_GRASS),
-            MaterialRules.condition(dirtLayer, INFECTED_DIRT)
-    );
-
-    MaterialRules.MaterialRule underwaterRule = MaterialRules.sequence(
-            MaterialRules.condition(topBlock, GRAVEL)
-    );
-
-    return MaterialRules.condition(
+        return MaterialRules.condition(
             MaterialRules.biome(ModBiomes.INFECTED_KEY),
-            MaterialRules.sequence(
-                    MaterialRules.condition(isAboveWater, landRule),
-                    underwaterRule
+            MaterialRules.condition(
+                caveFloor,
+                MaterialRules.condition(
+                    underground,
+                    INFECTED_GRASS
+                )
             )
-    );
-}
-    
+        );
+    }
 }
